1122586d9028b4cc624b7c85e7af21009c23cf5b
Ian van de Poll <ian.vd.poll@gmail.com>
2023-11-21 22:50:46 +0100

    Fix Clang const emplace_back illegal operation

--- src/rendering/RenderContext.cpp
+++ src/rendering/RenderContext.cpp
@@ -13,11 +13,11 @@
 #include <vulkan/vulkan_beta.h>
 #endif
 
-const std::vector<const char *> validationLayers = {
+std::vector<const char *> validationLayers = {
         "VK_LAYER_KHRONOS_validation"
 };
 
-const std::vector<const char *> deviceExtensions = {
+std::vector<const char *> deviceExtensions = {
         VK_KHR_SWAPCHAIN_EXTENSION_NAME
 };
 
