1c496bb960f239b5649f3886e2236a3bce1993ea
Ian van de Poll <ian.vd.poll@gmail.com>
2024-01-10 21:47:50 +0100

    Update application logic with controls

--- src/TraceApplication.cpp
+++ src/TraceApplication.cpp
@@ -12,26 +12,76 @@ void TraceApplication::Init() {
         m_ResizeRequested = true;
     };
 
+    m_MainWindow->OnMouseMovement = [this](double x, double y) {
+
+    };
+
+    m_MainWindow->OnKeyInput = [this](int key, int scanCode, int action, int mods) {
+        if(action == GLFW_REPEAT)
+            action = GLFW_PRESS;
+
+        keyState[key] = action;
+    };
+
     auto contextResources = m_Context.GetContextResources();
+    auto windowFrameSize = m_MainWindow->GetWindowFrameBuffer();
 
     m_Context.Init(m_MainWindow);
     m_Interface.Init(m_MainWindow, contextResources);
     m_FrameRenderer.Init(contextResources, m_MainWindow->GetWindowFrameBuffer());
+
+    m_PathTracer = new PathTracer("", windowFrameSize);
+
+    m_Camera = m_PathTracer->GetCamera();
+    m_Accumulator = m_PathTracer->GetAccumulator();
 }
 
-void TraceApplication::OnTick(double elapsedTime) {
-    Update();
+void TraceApplication::OnTick(float elapsedTime) {
+    Update(elapsedTime);
     Render();
-
-    // std::cout << "Tick happened with an elapsed time of : " << elapsedTime << std::endl;
 }
 
-void TraceApplication::Update() {
+void TraceApplication::Update(float elapsedTime) {
     if(m_ResizeRequested) {
-        m_Context.ResizeSwapChain(m_MainWindow->GetWindowFrameBuffer());
-        m_FrameRenderer.ResizeAccumulatorTexture(m_MainWindow->GetWindowFrameBuffer());
+        auto windowFrame = m_MainWindow->GetWindowFrameBuffer();
+
+        m_Context.ResizeSwapChain(windowFrame);
+        m_FrameRenderer.ResizeAccumulatorTexture(windowFrame);
+        m_Accumulator->Resize(windowFrame);
         m_ResizeRequested = false;
     }
+
+    if(keyState[GLFW_KEY_F1] == GLFW_PRESS && !controlStates.hasToggledEnableMouse) {
+        controlStates.enableMouse = !controlStates.enableMouse;
+        controlStates.hasToggledEnableMouse = true;
+    }
+
+    if(keyState[GLFW_KEY_F1] == GLFW_RELEASE && controlStates.hasToggledEnableMouse) {
+        controlStates.hasToggledEnableMouse = false;
+    }
+
+    if(keyState[GLFW_KEY_W] == GLFW_PRESS ) {
+        m_Camera->MoveForward(elapsedTime);
+    }
+
+    if(keyState[GLFW_KEY_A] == GLFW_PRESS) {
+        m_Camera->MoveLeft(elapsedTime);
+    }
+
+    if(keyState[GLFW_KEY_S] == GLFW_PRESS) {
+        m_Camera->MoveBackward(elapsedTime);
+    }
+
+    if(keyState[GLFW_KEY_D] == GLFW_PRESS) {
+        m_Camera->MoveRight(elapsedTime);
+    }
+
+    if(controlStates.enableMouse) {
+        m_Camera->ChangeOrientation(0, 0);
+        std::cout << m_Camera->m_Target.x << ", " << m_Camera->m_Target.y << ", " << m_Camera->m_Target.z  << std::endl;
+    }
+
+    m_FrameRenderer.CopyAccumulatorToTexture(m_Accumulator->GetImage(0));
 
     wgpuDeviceTick(*m_Context.GetContextResources().device);
 }
@@ -49,11 +99,15 @@ void TraceApplication::Render() {
 
     m_Context.SubmitCommandBuffer();
     m_Context.Present();
+
+    m_PathTracer->RenderFrame();
 }
 
 void TraceApplication::ShutDown() {
     m_Interface.Destroy();
     m_Context.Destroy();
+
+    delete m_Accumulator;
 
     Application::ShutDown();
 }
--- src/TraceApplication.h
+++ src/TraceApplication.h
@@ -10,21 +10,37 @@
 #include "core/Application.h"
 #include "rendering/RenderContext.h"
 #include "rendering/FrameRenderer.h"
+#include "tracer/Camera.h"
+#include "tracer/Accumulator.h"
+#include "tracer/PathTracer.h"
 
+struct ControlStates {
+    bool enableMouse;
+    bool hasToggledEnableMouse;
+};
 
 class TraceApplication : public Application {
 public:
     void Init() override;
-    void OnTick(double elapsedTime) override;
+    void OnTick(float elapsedTime) override;
     void ShutDown() override;
 private:
+    ControlStates controlStates {};
+
     RenderContext m_Context;
-    Interface m_Interface;
     FrameRenderer m_FrameRenderer;
+    Interface m_Interface;
+
+    PathTracer* m_PathTracer;
+    Accumulator* m_Accumulator;
+    Camera* m_Camera;
+
+    vec2f mousePosition;
+    uint8_t keyState[GLFW_KEY_LAST];
 
     bool m_ResizeRequested = false;
 
-    void Update();
+    void Update(float);
     void Render();
 };
 
